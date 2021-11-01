fdff82302f51e366a864fe15f97efe7e5c694e18
diegomurillocorrea <diego.murillo@elaniin.com>
2021-11-01 14:17:15 -0600

    Footer component initialized

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,38 @@
 const Footer = () => {
     return (
         <div>
-            
+            <div>
+                <div>
+                    <div>
+                        <p></p>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <p></p>
+                    </div>
+                    <div>
+                        <p></p>
+                    </div>
+                    <div>
+                        <p></p>
+                    </div>
+                    <div>
+                        <p></p>
+                    </div>
+                    <div>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
