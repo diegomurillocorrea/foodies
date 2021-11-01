56b1df940303281d57f302be4a1c34063ab75225
diegomurillocorrea <diego.murillo@elaniin.com>
2021-11-01 11:11:11 -0600

    Layout component finished

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import Open from "../components/icons/Open";
+import Closed from "../components/icons/Closed";
+
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div>
             <div className="font-noto-sans flex items-center justify-between px-5 py-10">
-                <div>
+                <div className="-mt-2">
                     <a 
                         href=""
                         className="font-druk-bold font-extrabold text-3xl"
@@ -25,8 +30,13 @@ const NavBar = () => {
                     </div>
                 </div>
                 <div className="xl:block">
-                    <button>
-                        Open
+                    <button 
+                        className="w-10 h-10" 
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        {isOpen ? 
+                        <Closed classes="fill-current text-black" /> : 
+                        <Open classes="stroke-current text-black" />}
                     </button>
                 </div>
             </div>
