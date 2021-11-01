0f00100fcb22d7fb21b73f99b58cfdb10c9344c9
diegomurillocorrea <diego.murillo@elaniin.com>
2021-11-01 10:44:57 -0600

    Icons added

---
+++ components/icons/Open.jsx
@@ -0,0 +1,13 @@
+const Open = ({ classes }) => {
+    return (
+        <div className={classes}>
+            <svg viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.25 13H22.75" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M3.25 6.5H22.75" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M3.25 19.5H22.75" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+        </div>
+    );
+};
+
+export default Open;
