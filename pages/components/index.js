77c50196e61b6bda36055f11c278824867747cb4
diegomurillocorrea <diego.murillo@elaniin.com>
2021-11-16 23:19:54 -0600

    Button component used and finished

--- pages/components/index.js
+++ pages/components/index.js
@@ -3,6 +3,8 @@ import NavBar from "../../components/NavBar";
 import Button from "../../components/Button";
 import Footer from "../../components/Footer";
 import ArrowRight from "../../components/icons/ArrowRight";
+import Truck from "../../components/icons/Truck";
+import Food from "../../components/icons/Food";
 
 const Components = () => {
     return (
@@ -27,6 +29,28 @@ const Components = () => {
                         label="Contáctanos"
                         iconR={<ArrowRight
                             classes="stroke-current"
+                        />}
+                        iconRClasses="group-hover:animate-bouncer"
+                    />
+                </div>
+            </div>
+            <div className="flex items-center justify-center space-x-20 py-10">
+                <div>
+                    <Button
+                        classes="font-syne-bold space-x-3 text-lg group"
+                        label="Para llevar"
+                        iconL={<Food
+                            classes="fill-current"
+                        />}
+                        iconRClasses="group-hover:animate-bouncer"
+                    />
+                </div>
+                <div>
+                    <Button
+                        classes="font-syne-bold space-x-3 text-lg group"
+                        label="Domicilio"
+                        iconL={<Truck
+                            classes="fill-current"
                         />}
                         iconRClasses="group-hover:animate-bouncer"
                     />
